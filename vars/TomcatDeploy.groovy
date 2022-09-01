abe7c5d1ee1a042495fb62e221bd09f052bd04af
Abhishek Srivastava <abhishek.srivastava121@gmail.com>
2022-09-01 19:00:30 +0530

    Update TomcatDeploy.groovy

--- vars/TomcatDeploy.groovy
+++ vars/TomcatDeploy.groovy
@@ -9,8 +9,8 @@ def call(credId,username,IpAddrs)
       IpAddrs.each{IpAddr->
   				sh "mv target/*.war target/abhishek.war"
         sh "scp -o StrictHostKeyChecking=no target/abhishek.war ${username}@${IpAddr}:/opt/tomcat9/webapps/"
-					sh "ssh ${username}@${IpAddr} /opt/tomcat9/bin/shutdown.sh"
-					sh "ssh ${username}@${IpAddr} /opt/tomcat9/bin/startup.sh"
+					//sh "ssh ${username}@${IpAddr} /opt/tomcat9/bin/shutdown.sh"
+					//sh "ssh ${username}@${IpAddr} /opt/tomcat9/bin/startup.sh"
       }
 		}
 }
