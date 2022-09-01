b7a380ee16005b65eb77a9f05416909695b1a576
Abhishek Srivastava <abhishek.srivastava121@gmail.com>
2022-09-01 20:01:03 +0530

    Update TomcatDeploy.groovy

--- vars/TomcatDeploy.groovy
+++ vars/TomcatDeploy.groovy
@@ -5,10 +5,11 @@
 
 def call(credId,username,IpAddrs)
 {
-    sshagent([credId]) {
+    sshagent(['Tomcat-dev']) {
       IpAddrs.each{IpAddr->
   				//sh "mv target/*.war target/abhishek.war"
-        sh "scp -o StrictHostKeyChecking=no target/myweb-8.3.3.war ${username}@${IpAddr}:/opt/tomcat9/webapps/"
+        //sh "scp -o StrictHostKeyChecking=no target/myweb-8.3.3.war ${username}@${IpAddr}:/opt/tomcat9/webapps/"
+	      sh "scp -o StrictHostKeyChecking=no target/myweb-8.3.3.war ec2-user@172.31.85.108:/opt/tomcat9/webapps/"
 					//sh "ssh ${username}@${IpAddr} /opt/tomcat9/bin/shutdown.sh"
 					//sh "ssh ${username}@${IpAddr} /opt/tomcat9/bin/startup.sh"
       }
